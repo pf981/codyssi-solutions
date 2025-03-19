b0a9ba4a620caa2a05225cbc6cb6169753402bd2
Paul Foster <paulfstr@gmail.com>
2025-03-19 16:25:44 +1100

    Complete 2025 03

--- 2025_contest/03.py
+++ 2025_contest/03.py
@@ -4,7 +4,7 @@ import re
 with open("2025_contest/03.txt") as f:
     inp = f.read()
 
-nums = [int(b) - int(a) + 1 for a, b in re.findall(r'(\d+)-(\d+)', inp)]
+nums = [int(b) - int(a) + 1 for a, b in re.findall(r"(\d+)-(\d+)", inp)]
 
 answer1 = sum(nums)
 print(answer1)
@@ -14,7 +14,7 @@ print(answer1)
 
 piles = []
 for line in inp.splitlines():
-    a, b, c, d = (int(num) for num in re.findall(r'\d+', line))
+    a, b, c, d = (int(num) for num in re.findall(r"\d+", line))
     piles.append(set(range(a, b + 1)) | set(range(c, d + 1)))
 
 answer2 = sum(len(pile) for pile in piles)
