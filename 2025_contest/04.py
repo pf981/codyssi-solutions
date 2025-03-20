b604779d99f5d286cbb7a064746b4745c8ac4002
Paul Foster <paulfstr@gmail.com>
2025-03-20 22:06:43 +1100

    Tidy code

--- 2025_contest/04.py
+++ 2025_contest/04.py
@@ -2,51 +2,44 @@ import re
 
 
 with open("2025_contest/04.txt") as f:
-    inp = f.read()
+    lines = f.read().splitlines()
 
-# inp = '''NNBUSSSSSDSSZZZZMMMMMMMM
-# PWAAASYBRRREEEEEEE
-# FBBOFFFKDDDDDDDDD
-# VJAANCPKKLZSSSSSSSSS
-# NNNNNNBBVVVVVVVVV'''
 
-answer1 = sum(ord(c) - ord('A') + 1 for c in inp if c != '\n')
+def compute_memory(s):
+    return sum(int(c) if c.isdigit() else (ord(c) - ord('A') + 1) for c in s)
+
+answer1 = sum(compute_memory(s) for s in lines)
 print(answer1)
 
 
 # Part 2
 
 answer2 = 0
-for line in inp.splitlines():
+for line in lines:
     keep = len(line) // 10
     s = line[:keep] + str(len(line) - keep - keep) + line[-keep:]
-    memory = sum(int(c) if c.isdigit() else (ord(c) - ord('A') + 1) for c in s)
-    answer2 += memory
+    answer2 += compute_memory(s)
 
 print(answer2)
 
 
 # Part 3
 
-# inp = '''NNBUSSSSSDSSZZZZMMMMMMMM
-# PWAAASYBRRREEEEEEE
-# FBBOFFFKDDDDDDDDD
-# VJAANCPKKLZSSSSSSSSS
-# NNNNNNBBVVVVVVVVV'''
-
 answer3 = 0
-for line in inp.splitlines():
+for line in lines:
     s = []
     count = 1
     char = line[0]
+
     for c in line[1:] + ';':
         if c != char:
             s.extend(str(count))
             s.append(char)
             count = 0
+
         char = c
         count += 1
-    memory = sum(int(c) if c.isdigit() else (ord(c) - ord('A') + 1) for c in s)
-    answer3 += memory
+
+    answer3 += compute_memory(s)
 
 print(answer3)
