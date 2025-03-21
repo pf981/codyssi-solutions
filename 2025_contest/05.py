e50650e9575d92bb30c06375f02dde7906fe44e4
pf981 <paulfstr@gmail.com>
2025-03-21 21:28:19 +1100

    Tidy code

--- 2025_contest/05.py
+++ 2025_contest/05.py
@@ -2,19 +2,20 @@ import re
 
 
 with open("2025_contest/05.txt") as f:
-    lines = f.read().splitlines()
+    inp = f.read()
 
-coords = [(int(line.split(', ')[0][1:]), int(line.split(', ')[1][:-1])) for line in lines]
-
+coords = [(int(x), int(y)) for x, y in re.findall(r'(-?\d+), (-?\d+)', inp)]
 closest = min(abs(x) + abs(y) for x, y in coords)
 furthest = max(abs(x) + abs(y) for x, y in coords)
+
 answer1 = furthest - closest
 print(answer1)
 
 
 # Part 2
+
 _, x1, y1 = min((abs(x) + abs(y), x, y) for x, y in coords)
-answer2 = min((abs(x - x1) + abs(y - y1), x, y) for x, y in coords if (x, y) != (x1, y1))
+answer2, _, _ = min((abs(x - x1) + abs(y - y1), x, y) for x, y in coords if (x, y) != (x1, y1))
 print(answer2)
 
 
