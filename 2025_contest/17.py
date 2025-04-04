49767777254125336e14e71b440ef022932a08c9
Paul Foster <paulfstr@gmail.com>
2025-04-04 16:19:00 +1100

    Complete 2025 day 17

--- 2025_contest/17.py
+++ 2025_contest/17.py
@@ -1,3 +1,4 @@
+import collections
 import functools
 import re
 
@@ -34,42 +35,163 @@ print(answer1)
 
 # Part 2
 
-
-# inp = """S1 : 0 -> 6 : FROM START TO END
-# S2 : 2 -> 4 : FROM S1 TO S1
-# S3 : 3 -> 5 : FROM S2 TO S1
-
-# Possible Moves : 1, 2"""
-
-# staircases_s, possible_moves_s = inp.split("\n\n")
-# possible_moves = [
-#     int(num) for num in re.findall(r"-?[0-9]+", possible_moves_s.splitlines()[0])
-# ]
-
-# # re.match(
-# #     r"(S\d+) : (\d+) -> (\d+) : FROM (.+) TO (.+)", "S1 : 0 -> 6 : FROM START TO END"
-# # ).groups()
-# import collections
-
-# m = collections.defaultdict(
-#     list
-# )  # (staircase_id, stair) -> [(staircaes_id, stair), ...]
-
-# staircases = []
-# for line in staircases_s.splitlines():
-#     staircase_id, start, end, from_staircase, to_staircase = re.match(
-#         r"(S\d+) : (\d+) -> (\d+) : FROM (.+) TO (.+)", line
-#     ).groups()
-
-#     print(staircase_id, start, end, from_staircase, to_staircase)
-#     staircases.append(
-#         (staircase_id, int(start), int(end), from_staircase, to_staircase)
-#     )
+staircases_s, possible_moves_s = inp.split("\n\n")
+possible_moves = [
+    int(num) for num in re.findall(r"-?[0-9]+", possible_moves_s.splitlines()[0])
+]
 
 
-# for staircase_id, start, end, from_staircase, to_staircase in staircases:
-#     for step in range(start, end):
-#         m[(staircase_id, step)].append((staircase_id, step + 1))
+staircases = []
+for line in staircases_s.splitlines():
+    staircase_id, start, end, from_staircase, to_staircase = re.match(
+        r"S(\d+) : (\d+) -> (\d+) : FROM (.+) TO (.+)", line
+    ).groups()
 
-# # FROM
-# # TO
+    staircases.append(
+        (
+            int(staircase_id),
+            int(start),
+            int(end),
+            int(from_staircase[1:]) if from_staircase != "START" else from_staircase,
+            int(to_staircase[1:]) if to_staircase != "END" else to_staircase,
+        )
+    )
+
+
+start_pos = None
+end_pos = None
+m = collections.defaultdict(
+    list
+)  # (staircase_id, stair) -> [(staircaes_id, stair), ...]
+
+for staircase_id, start, end, from_staircase, to_staircase in staircases:
+    for step in range(start, end):
+        m[(staircase_id, step)].append((staircase_id, step + 1))
+
+    if from_staircase == "START":
+        start_pos = (staircase_id, start)
+    else:
+        m[(from_staircase, start)].append((staircase_id, start))
+
+    if to_staircase == "END":
+        end_pos = (staircase_id, end)
+    else:
+        m[(staircase_id, end)].append((to_staircase, end))
+
+
+def get_next_positions(pos, move):
+    if move == 0:
+        return {pos}
+
+    next_positions = set()
+    for pos2 in m[pos]:
+        next_positions.update(get_next_positions(pos2, move - 1))
+    return next_positions
+
+
+@functools.cache
+def count_ways(pos):
+    if pos == end_pos:
+        return 1
+    if pos[1] > end_pos[1]:
+        return 0
+
+    result = 0
+    moves = {pos2 for move in possible_moves for pos2 in get_next_positions(pos, move)}
+    for pos2 in moves:
+        result += count_ways(pos2)
+    return result
+
+
+answer2 = count_ways(start_pos)
+print(answer2)
+
+
+# Part 3
+
+
+staircases_s, possible_moves_s = inp.split("\n\n")
+possible_moves = [
+    int(num) for num in re.findall(r"-?[0-9]+", possible_moves_s.splitlines()[0])
+]
+
+
+staircases = []
+for line in staircases_s.splitlines():
+    staircase_id, start, end, from_staircase, to_staircase = re.match(
+        r"S(\d+) : (\d+) -> (\d+) : FROM (.+) TO (.+)", line
+    ).groups()
+
+    staircases.append(
+        (
+            int(staircase_id),
+            int(start),
+            int(end),
+            int(from_staircase[1:]) if from_staircase != "START" else from_staircase,
+            int(to_staircase[1:]) if to_staircase != "END" else to_staircase,
+        )
+    )
+
+
+start_pos = None
+end_pos = None
+m = collections.defaultdict(
+    list
+)  # (staircase_id, stair) -> [(staircaes_id, stair), ...]
+
+for staircase_id, start, end, from_staircase, to_staircase in staircases:
+    for step in range(start, end):
+        m[(staircase_id, step)].append((staircase_id, step + 1))
+
+    if from_staircase == "START":
+        start_pos = (staircase_id, start)
+    else:
+        m[(from_staircase, start)].append((staircase_id, start))
+
+    if to_staircase == "END":
+        end_pos = (staircase_id, end)
+    else:
+        m[(staircase_id, end)].append((to_staircase, end))
+
+
+def get_next_positions(pos, move):
+    if move == 0:
+        return {pos}
+
+    next_positions = set()
+    for pos2 in m[pos]:
+        next_positions.update(get_next_positions(pos2, move - 1))
+    return next_positions
+
+
+@functools.cache
+def count_ways(pos):
+    if pos == end_pos:
+        return 1
+    if pos[1] > end_pos[1]:
+        return 0
+
+    result = 0
+    moves = {pos2 for move in possible_moves for pos2 in get_next_positions(pos, move)}
+    for pos2 in moves:
+        result += count_ways(pos2)
+    return result
+
+
+path = [f"S{start_pos[0]}_{start_pos[1]}"]
+pos = start_pos
+rank = 0
+while pos != end_pos:
+    moves = {pos2 for move in possible_moves for pos2 in get_next_positions(pos, move)}
+    print(f"{sorted(moves)=}")
+    for pos2 in sorted(moves):
+        ways = count_ways(pos2)
+        if rank + ways < 100000000000000000000000000000:
+            rank += ways
+        else:
+            break
+    path.append(f"S{pos2[0]}_{pos2[1]}")
+    pos = pos2
+
+answer3 = "-".join(path)
+print(answer3)
