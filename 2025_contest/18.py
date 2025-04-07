6da9285bf9eae3630246c8e2428b011a9b1b60ea
Paul Foster <paulfstr@gmail.com>
2025-04-07 19:09:39 +1000

    2025 18 skeleton

---
+++ 2025_contest/18.py
@@ -0,0 +1,22 @@
+import collections
+import functools
+import re
+
+
+with open("2025_contest/18.txt") as f:
+    inp = f.read()
+
+
+print(answer1)
+
+
+# Part 2
+
+
+print(answer2)
+
+
+# Part 3
+
+
+print(answer3)
